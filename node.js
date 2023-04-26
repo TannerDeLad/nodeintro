d019362d1011f41da39b501c98f7650573120c07
LADOUCEUR <tl0976794@dt038568.otc.edu>
2023-04-26 12:53:41 -0500

    added

--- node.js
+++ node.js
@@ -1,8 +1,19 @@
 import chalk from 'chalk';
-
+import axios from 'axios';
 console.log(chalk.blue('Hello world!'));
 console.log(`
 CPU: ${chalk.red('90%')}
 RAM: ${chalk.green('40%')}
 DISK: ${chalk.yellow('70%')}
 `);
+// naming of the function when importing 
+import scripts from './scripts.js';
+scripts();
+// when using an already named function yo umust use curly braces 
+import {configs} from './scripts.js';
+configs();
+
+axios.get("https://dog.ceo/api/breeds/image/random")
+.then ((obj) => {
+    console.log(obj.data);
+})
---
+++ scripts.js
@@ -0,0 +1,7 @@
+export default function() {
+    console.log("Thee function");
+}
+
+export function configs(){
+    console.log("OTher function");
+}
