19493d1099828fda30012653323473da1e8fb788
LADOUCEUR <tl0976794@dt038568.otc.edu>
2023-04-26 12:28:21 -0500

    initial

---
+++ node.js
@@ -0,0 +1,8 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
+console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Tanner 
+## Intro to Node Projecting
